0f29bf3fb1ffcf64fca811f1e6bfa082b4e14bdf
Erin DeVries <erindevrieswebdev@gmail.com>
2017-01-30 19:17:08 -0600

    Regular Expressions added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 import webapp2
+import re
 
 page_header = """
 <!DOCTYPE html>
@@ -31,6 +32,7 @@ page_header = """
 
 """
 
+
 # html boilerplate for the bottom of every page
 page_footer = """
 </body>
@@ -43,41 +45,49 @@ info_form = """
     <label>
     Username:
     <input type="text" name="name"/>
+    <div class="error">{error_username}</div>
     </label>
     <br> <br>
     <label>
     Password:
     <input type="password" name="password"/>
+    <div class="error">{error_password}</div}
     </label>
     <br> <br>
     <label>
     Verify Password:
     <input type="password" name="verify"/>
+    <div class="error">{error_verify}</div>
     </label>
     <br> <br>
     <label>
     Email(optional)
     <input type="text" name="email"/>
+    <div class="error">{error_email}</div>
     </label>
     <br> <br>
     <input type="submit" value="Submit">
 </form>
 """
-def valid_un(name):
-    for i in name:
-        if i == " ":
-            return False
-        else:
-            return True
 
-def valid_password(password, verify):
-    if password == verify:
-        return True
-    else:
-        return False
+def valid_un(name):
+    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    return USER_RE.match(name)
+
+
+def valid_match(password, verify):
+    Password_RE = re.compile("^.{3,20}$")
+    return (password == verify)
+
+def valid_password(password):
+    Password_RE = re.compile("^.{3,20}$")
+    return Password_RE.match(password)
+
+
 
 def valid_email(email):
-    return True
+    email_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+    return email_RE.match(email)
 
 class Signup(webapp2.RequestHandler):
     def get(self):
@@ -87,13 +97,14 @@ class Signup(webapp2.RequestHandler):
 
     def post(self):
         user_name = valid_un(self.request.get("name"))
-        user_password = valid_password(self.request.get("password"))
+        user_match = valid_match( self.request.get("password"), self.request.get("verify") )
         user_email = valid_email(self.request.get("email"))
+        user_password = valid_password(self.request.get("password"))
 
-        if not (user_name and user_password and user_email):
-            self.response.write(info_form)
+        if not (user_name and user_password and user_email and user_match):
+            self.response.write(signup_header + info_form)
         else:
-            self.response.write("Thanks!")
+            self.response.write("<div style='color:red;'>Thanks!</div>")
 
 app = webapp2.WSGIApplication([
     ('/', Signup)
